169198268f432e767240d1dab0d9d4010bfad1a5
Sidy Diallo <sidiallo@bess-f2r2s3.clusters.42paris.fr>
2025-02-26 16:02:14 +0100

    add ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,14 +6,31 @@
 /*   By: sidiallo <sidiallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:23:29 by sidiallo          #+#    #+#             */
-/*   Updated: 2025/02/25 18:24:36 by sidiallo         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:00:15 by sidiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include<fstream>
+#include<string>
 
 
+std::string replaceString(std::string const s1, std::string const s2 , std::string line)
+{
+    std::string newLine;
+    size_t pos = 0;
+    size_t found;
+    
+    while ((found = line.find(s1, pos)) != std::string::npos) {
+        newLine += line.substr(pos, found - pos);
+        newLine += s2;                      
+        pos = found + s1.length();
+    }
+
+    newLine += line.substr(pos);
+
+    return (newLine);
+}
 
 
 int main(int ac, char **av)
@@ -23,24 +40,22 @@ int main(int ac, char **av)
         std::string filename = av[1];
         std::string s1 = av[2];
         std::string s2 = av[3];
-        std::string word = s1;
+        std::string line;
         
         
-        std::ifstream ifs(filename);
+        std::ifstream ifs(filename.c_str());
         if(!ifs)
             return(1);
-        std::ofstream ofs(filename + ".replace");
+            
+        std::ofstream ofs((filename + ".replace").c_str());
          if(!ofs) 
             return (1);
-            
-        while(word != "\0")
+        
+        while(std::getline(ifs,line))
         {
-            
-        }
-        
-        
-
-            
+            line  = replaceString(s1,s2,line);
+            ofs << line + "\n";
+        }   
         ifs.close();
         ofs.close();
     }
